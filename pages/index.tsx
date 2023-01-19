c45e289fa8a59a55f4cf184e093ec1e721d226ad
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-01-19 17:15:00 -0600

    Added layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <div className="flex h-screen w-screen items-center justify-center">
-      <p className="text-bold">Hello World!</p>
-    </div>
-  )
+  return <div>About Me</div>
 }
