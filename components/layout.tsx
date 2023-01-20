8e87ea4d7543a46ba0dd98114677c4b4c412130b
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-01-20 15:19:22 -0600

    Pass attributes to NavItem

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,13 @@
-function NavItem() {
+type NavItemProps = {
+  title: string
+  url: string
+}
+
+function NavItem(props: NavItemProps) {
+  const {title, url} = props
   return (
     <li>
-      <a className="block px-3 py-2 transition hover:text-teal-500" href="/">About Me</a>
+      <a className="block px-3 py-2 transition hover:text-teal-500" href={url}>{title}</a>
     </li>
   )
 }
@@ -11,9 +17,9 @@ function Navbar() {
     <div className="flex justify-center mx-auto max-w-7xl h-16 pt-6">
       <nav>
         <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
-          <NavItem />
-          <NavItem />
-          <NavItem /> 
+          <NavItem title={"About Me"} url={"/"}  />
+          <NavItem title={"Projects"} url={"/projects"} />
+          <NavItem title={"Essays"} url={"/essays"} /> 
         </ul>
       </nav>
     </div>
