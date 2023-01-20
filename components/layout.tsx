4cb581e38596fcfd89ba62a6c95e8a2fecb833cd
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-01-20 14:27:39 -0600

    Added NavItem

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,7 +1,30 @@
+function NavItem() {
+  return (
+    <li>
+      <a className="block px-3 py-2 transition hover:text-teal-500" href="/">About Me</a>
+    </li>
+  )
+}
+
+function Navbar() {
+  return (
+    <div className="flex justify-center mx-auto max-w-7xl h-16 pt-6">
+      <nav>
+        <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur">
+          <NavItem />
+          <NavItem />
+          <NavItem /> 
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
 export default function Layout({ children }: any) {
-    return (
-      <>
-        <main>{children}</main>
-      </>
-    )
-  }
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+    </>
+  )
+}
