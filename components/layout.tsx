0b5b6c8f9c8b99e9755d7fa3fbf393619d10bd27
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-01-25 17:07:09 -0600

    Added footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -31,11 +31,35 @@ function Navbar(props: NavbarProps) {
   )
 }
 
+type FooterLinkProps = {
+  text: string
+  url: string
+}
+
+function FooterLink(props: FooterLinkProps) {
+  let {url, text} = props
+  return <a className="transition hover:text-teal-500" href={url}>{text}</a>
+}
+
+function Footer() {
+  return <footer className="pt-10 px-8 pb-16 border-t">
+    <div className="flex justify-between gap-6">
+      <div className="flex gap-6 text-sm font-medium text-zinc-600">
+        <FooterLink text={"About Me"} url={"/"} />
+        <FooterLink text={"Projects"} url={"/projects"} />
+        <FooterLink text={"Essays"} url={"/essays"} />
+      </div>
+      <p className="text-sm text-zinc-400">© 2023 Grace Huang. All rights reserved.</p>
+    </div>
+  </footer>
+}
+
 export default function Layout({ children }: any) {
   return (
     <>
       <Navbar pageId={children.props.pageId} />
       <main>{children}</main>
+      <Footer />
     </>
   )
 }
