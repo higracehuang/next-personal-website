fcd4db7d588ec6297623d73df22cf543bffa6dc7
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-01-27 13:56:11 -0600

    Updated the text

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -33,7 +33,7 @@ function ProjectItem(props: ProjectItemProps) {
           <div className="font-bold text-xl mb-2">{name}</div>
         </div>
         <div className="px-6 pb-4">
-          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">🔗 {urlDisplay}</span>
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">🔗{urlDisplay}</span>
         </div>
       </div>
     </a>
@@ -43,19 +43,19 @@ function ProjectItem(props: ProjectItemProps) {
 export default function Projects() {
   return <div className="mt-16 px-8">
     <header>
-      <h1 className="font-bold text-4xl text-zinc-800">Creative Things I Do Now</h1>
-      <p className="text-base mt-6 text-zinc-600">I work on a number of small projects concurrently.</p>
+      <h1 className="font-bold text-4xl text-zinc-800">Things I Do</h1>
+      <p className="text-base mt-6 text-zinc-600">I have been working on a number of small creative projects</p>
     </header>
     <div className="mt-16">
       <h2 className="text-2xl">Apps</h2>
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-16 mt-8">
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-12 gap-y-16 mt-8">
         <ProjectItem name={"TallyCoin: Tracking Chores And Rewards"} url={"https://apps.apple.com/us/app/tallycoin/id1633932632"} urlDisplay={"App Store"} imageSrc={imageAppTallyCoin} />
         <ProjectItem name={"RedacApp: Redact Text In Images"} url={"https://gracehuang.gumroad.com/l/redac"} urlDisplay={"gumroad.com"} imageSrc={imageAppRedacApp} />
       </ul>
     </div>
     <div className="mt-16">
       <h2 className="text-2xl">Books</h2>
-      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-12 gap-y-16 mt-8">
+      <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-x-12 gap-y-16 mt-8">
         <ProjectItem name={"Building macOS apps with SwiftUI: A Practical Learning Guide"} url={"https://www.amazon.com/gp/product/B0BP5P9H31"} urlDisplay={"amazon.com"} imageSrc={imageBookMacOSApp} />
         <ProjectItem name={"Nail A Coding Interview: Six-Step Mental Framework"} url={"https://gracehuang.gumroad.com/l/coding-interview"} urlDisplay={"gracehuang.gumroad.com"} imageSrc={imageBookCodingInterview} />
         <ProjectItem name={"A Practical Guide to Writing a Software Tech Design Doc"} url={"https://gracehuang.gumroad.com/l/mqmUt"} urlDisplay={"gracehuang.gumroad.com"} imageSrc={imageBookTDD} />
