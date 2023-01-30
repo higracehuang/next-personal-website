3bc24325d5c2ac03f4f4d3de6dc521007804c312
higracehuang <117108383+higracehuang@users.noreply.github.com>
2023-01-30 15:56:55 -0600

    Added metadata for About Me

--- components/layout.tsx
+++ components/layout.tsx
@@ -61,6 +61,13 @@ export default function Layout({ children }: any) {
     <>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <title>{children.props.metadata.title}</title>
+        <meta name="description" content={children.props.metadata.description} />
+        {/* Open Graph tags */}
+        <meta property="og:title" content={children.props.metadata.title} />
+        <meta property="og:description" content={children.props.metadata.description} />
+        <meta property="og:image" content={children.props.metadata.openGraph.image} />
+        <meta property="og:url" content={children.props.metadata.openGraph.url} />
       </Head>
       <Navbar pageId={children.props.pageId} />
       <main>{children}</main>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
-import Head from "next/head"
-
 export async function getStaticProps() {
   return {
     props: {
-      pageId: "about_me"
+      pageId: "about_me",
+      metadata: {
+        title: "Grace Huang",
+        description: "Discover the work of Grace Huang on her personal website. ",
+        openGraph: {
+          image: "https://example.com/image.jpg",
+          url: "https://example.com"
+        }
+      }
     }
   }
 }
 
 export default function Home() {
   return <div className="mt-16 px-8">
-    <Head>
-      <title>Grace Huang</title>
-      <meta name="description" content="Discover the work of Grace Huang on her personal website. Here you will find a curated collection of her essays and projects, including her thoughts on various topics and the details of her completed and ongoing projects." />
-    </Head>
     <header>
       <h1 className="font-bold text-4xl text-zinc-800">I&apos;m Grace</h1>
     </header>
